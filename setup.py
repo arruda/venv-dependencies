c574735430dc4d4bcfeb038aebc049de84da99ee
Felipe Pontes <arruda@uniriotec.com>
2011-10-26 16:07:55 -0200

    removing read funcion from setup, so now it works nice with pip

--- setup.py
+++ setup.py
@@ -8,14 +8,13 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-structurer_dir = 'venv_dependencies'
 data_folder= []
 data_files = ['README.rst']
 
 
 setup(
     name = "venv_dependencies",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "Felipe Arruda Pontes",
     author_email = "contato@arruda.blog.br",
     description = ("Easy to install any dependencies in a virtualenviroment(without making symlinks by hand and etc...)"),
@@ -26,7 +25,8 @@ setup(
     scripts = ['venv_dependencies/bin/link_venv.py'],
     data_files = data_files,
     install_requires=[],
-    long_description=read('README.rst'),
+    long_description="""This app helps to create symbolics links for any module from your original python to your current virtual enviroment.
+This way you don't need to do the links by your self using bash, just use this app.""",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
